e65b2f5ca6f3618345d66c4f60b37f0ea795d989
Ricardo MONLA <rmonla@gmail.com>
2017-12-29 00:44:59 -0300

    Ver 1.3 Python -> Agrego opción listar directorios.

--- ecptdorFotos.py
+++ ecptdorFotos.py
@@ -3,14 +3,18 @@ from os.path import isdir, isfile, join
 import re
 
 def lstRgx( 
+    tpo = "d",      # Tipo de búsqueda f-> Archivos, d-> Directorios.
     pth = getcwd(), # String del Path donde se realiza la búsqueda.
     rgx = ""        # String con los comodines regex (Ej: ".*(IMG-).*").
     ):
-    lst = [a for a in listdir(pth) if isfile(join(pth, a))]
-    if not rgx == "":
+    lst = [d for d in listdir(pth) if isdir(join(pth, d))]
+    if tpo != "d"
+        lst = [f for f in listdir(pth) if isfile(join(pth, f))]
+    if rgx != "":
         rgx = re.compile( rgx )
         lst = [m.group(0) for l in lst for m in [rgx.search(l)] if m]
     return lst
 
 # print lstRgx('.', ".*(IP).*")
+# print lstRgx()
 
